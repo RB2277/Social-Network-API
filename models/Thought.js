bfb0e3e24348d8579c181dcdca6346428dac31d2
RB <jsriley24505@gmail.com>
2024-03-04 16:56:48 -0600

    setup reactionSchema within thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 
+const reactionSchema = new mongoose.Schema({
+  reactionID: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId()},
+  reactionBody: { type: String, required: true, maxLength: 280},
+  username: {type: String, required: true},
+  createdAt: { type: Date, default: Date.now },
+});
+
+
+
+
 const thoughtSchema = new mongoose.Schema({
   thoughtText: { type: String, required: true, minLength: 1, maxLength: 280},
   createdAt: { type: String, required: true, unique: true},
   //TODO: Use a getter method to format the timestamp on query
   lastAccessed: { type: Date, default: Date.now },
-  //TODO: Figure out how to link this
   username: {type: String, required: true},
-  reactions: []
+  reactions: [{reactionSchema}]
 });
 
 //TODO: Schema Settings
@@ -16,6 +25,6 @@ const thoughtSchema = new mongoose.Schema({
 
 
 
-const User = mongoose.model('user', userSchema);
+const Thought = mongoose.model('thought', thoughtSchema);
 
-module.exports = User
+module.exports = Thought
