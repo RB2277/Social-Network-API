87df3fb0580cec0fdf47de2b9e64cea5cb1a8ffe
RB <jsriley24505@gmail.com>
2024-03-05 10:27:41 -0600

    setup addFriend controller method

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,6 +54,41 @@ async deleteUser(req, res) {
     } catch (err) {
         res.status(500).json(err)
     }
+},
+
+async addFriend(req, res) {
+    try {
+        const { userId, friendId } = req.params
+    const user = await User.findById(userId)
+    if(!user){
+        return res.status(404).json({message: 'No user with that ID exists'})
+    }
+
+
+    user.friends.push(friendId)
+    await user.save()
+    res.json({message: "Friend added!", user})
+    } catch(err) {
+        res.status(500).json(err)
+    }
+}, 
+
+async removeFriend(req, res) {
+    try{
+    const { thoughtId, reactionId } = req.params
+    console.log(thoughtId)
+    console.log(reactionId)
+
+    const thought = await Thought.findById(thoughtId)
+    if(!thought){
+        return res.status(404).json({message: 'No thought with that ID exists'})
+    }
+    thought.reactions = thought.reactions.filter(reaction => !reaction.reactionID.equals(reactionId))
+   await thought.save()
+   res.json({message: "Your reaction has been removed", thought})
+    } catch(err) {
+        res.status(500).json(err)
+    }
 }
 
 
